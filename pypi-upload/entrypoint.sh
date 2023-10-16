56db463d75327af65af8255c9a13a78a177a31e6
César Román <cesar@coatl.dev>
2023-10-16 14:28:48 -0700

    feat: add pypi-upload (#18)

---
+++ pypi-upload/entrypoint.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# shellcheck source=/dev/null
+
+set -Eeuo pipefail
+
+export PATH="/usr/bin:${PATH}"  # To find `id`
+source /etc/profile  # Makes python and other executables findable
+
+python -m build
+
+if [[ ${INPUT_CHECK,,} != "no" ]] ; then
+    python -m twine check dist/*
+fi
+
+if [[ ${INPUT_REGISTER,,} != "no" ]] ; then
+    TWINE_USERNAME="$INPUT_USERNAME" \
+    TWINE_PASSWORD="$INPUT_PASSWORD" \
+    TWINE_REPOSITORY_URL="$INPUT_URL" \
+        python -m twine register dist/*.tar.gz
+fi
+
+if [[ ${INPUT_UPLOAD,,} != "no" ]] ; then
+    TWINE_USERNAME="$INPUT_USERNAME" \
+    TWINE_PASSWORD="$INPUT_PASSWORD" \
+    TWINE_REPOSITORY_URL="$INPUT_URL" \
+    python -m twine upload --verbose dist/*
+fi
