46989dca678adbdbe65eef8c0ff572b0bcabfe83
César Román <cesar@coatl.dev>
2023-10-16 11:02:21 -0700

    feat: add pre-commit-autoupdate (#13)

---
+++ pre-commit-autoupdate/pre-commit-autoupdate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z "$INPUT_SKIP_REPOS" ]; then
+    while IFS='' read -r repo || [ -n "${repo}" ]; do
+        pre-commit autoupdate --repo "$repo"
+    done < <(
+        grep -Eo 'https://[a-zA-Z0-9./?=_%:-]*' .pre-commit-config.yaml
+    )
+else
+    while IFS='' read -r repo || [ -n "${repo}" ]; do
+        pre-commit autoupdate --repo "$repo"
+    done < <(
+        grep -Eo 'https://[a-zA-Z0-9./?=_%:-]*' .pre-commit-config.yaml |
+        grep -iEwv "$INPUT_SKIP_REPOS"
+    )
+fi
