01f46e73066d3a532e7e612f11d8191bd2f9f636
César Román <cesar@coatl.dev>
2023-10-24 16:57:22 +0200

    refactor(pre-commit-autoupdate): remove leading white spaces

--- pre-commit-autoupdate/generate-commit-file.sh
+++ pre-commit-autoupdate/generate-commit-file.sh
@@ -5,5 +5,5 @@ echo "build(deps): coatl-dev pre-commit autoupdate" > "${RUNNER_TEMP}/commit.txt
     echo ""
     echo "updates:"
     git diff --unified=1 .pre-commit-config.yaml |
-    grep 'https://[a-zA-Z0-9./?=_%:-]*' | sed -e 's/ \{2,\}/  /g'
+    grep 'https://[a-zA-Z0-9./?=_%:-]*' | sed -e 's/ \{2,\}//g'
 } >> "${RUNNER_TEMP}/commit.txt"
