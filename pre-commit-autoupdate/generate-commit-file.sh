b9c3777e634f899273ca3f48fbc30ee815604040
César Román <cesar@coatl.dev>
2023-10-24 05:43:02 +0200

    refactor(pre-commit-autoupdate): remove relative path

--- pre-commit-autoupdate/generate-commit-file.sh
+++ pre-commit-autoupdate/generate-commit-file.sh
@@ -4,6 +4,6 @@ echo "build(deps): coatl-dev pre-commit autoupdate" > "${RUNNER_TEMP}/commit.txt
 {
     echo ""
     echo "updates:"
-    git diff --unified=1 ../.pre-commit-config.yaml |
+    git diff --unified=1 .pre-commit-config.yaml |
     grep 'https://[a-zA-Z0-9./?=_%:-]*' | sed -e 's/ \{2,\}/  /g'
 } >> "${RUNNER_TEMP}/commit.txt"
