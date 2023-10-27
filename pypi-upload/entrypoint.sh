521f68ffd0d9561a7b42f08a81297133a3484354
César Román <cesar@coatl.dev>
2023-10-27 23:11:36 +0200

    refactor(pypi-upload): add python-version input
    allow users to select their desired python version

--- pypi-upload/entrypoint.sh
+++ pypi-upload/entrypoint.sh
@@ -10,13 +10,6 @@ if [[ ${INPUT_CHECK,,} != "no" ]] ; then
     python -m twine check dist/*
 fi
 
-if [[ ${INPUT_REGISTER,,} != "no" ]] ; then
-    TWINE_USERNAME="$INPUT_USERNAME" \
-    TWINE_PASSWORD="$INPUT_PASSWORD" \
-    TWINE_REPOSITORY_URL="$INPUT_URL" \
-        python -m twine register dist/*.tar.gz
-fi
-
 if [[ ${INPUT_UPLOAD,,} != "no" ]] ; then
     TWINE_USERNAME="$INPUT_USERNAME" \
     TWINE_PASSWORD="$INPUT_PASSWORD" \
