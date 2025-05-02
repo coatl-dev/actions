66b3f421931d0311f8ea7c788d386443ffa0138d
César Román <cesar@coatl.dev>
2025-05-02 07:14:22 -0700

    feat: add uv-pip-compile action

---
+++ uv-pip-compile/action.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# shellcheck source=/dev/null
+
+export PATH="/usr/bin:${PATH}"  # To find `id`
+source /etc/profile  # Makes python and other executables findable
+
+function process_file() {
+  local file="$1"
+  command=$(grep -m 1 "#    uv pip compile" "$file" | sed "s/#    uv pip compile/uv pip compile --upgrade/")
+  eval "$command"
+}
+
+if [ -d "${INPUT_PATH}" ]; then
+  cd "${INPUT_PATH}" || exit
+  for file in *.txt; do
+    if [ -f "$file" ]; then
+      process_file "$file"
+    fi
+  done
+elif [ -f "${INPUT_PATH}" ]; then
+  process_file "${INPUT_PATH}"
+else
+  exit 1
+fi
