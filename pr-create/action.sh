bf05725efa33a5e5501aab7bd473e9ae86a073ef
César Román <cesar@coatl.dev>
2025-05-02 07:53:22 -0700

    feat: add uv-pip-compile action (#86)

---
+++ pr-create/action.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ -n "$INPUT_TITLE" ] && [ -n "$INPUT_BODY_FILE" ]; then
+    # Use title and body-file (if provided)
+    gh pr create \
+        --title "$INPUT_TITLE" \
+        --body-file "$INPUT_BODY_FILE"
+elif [ -n "$INPUT_TITLE" ] && [ -n "$INPUT_BODY" ]; then
+    # Use title and body (if provided)
+    gh pr create \
+        --title "$INPUT_TITLE" \
+        --body "$INPUT_BODY"
+else
+    # Use commit info for title and body
+    gh pr create --fill
+fi
+
+if [ "$INPUT_AUTO_MERGE" == "yes" ] && [ "$INPUT_DELETE_BRANCH" == "yes" ]; then
+    # Merge pull request and delete branch
+    gh pr merge \
+        --auto \
+        --delete-branch \
+        --squash
+elif [ "$INPUT_AUTO_MERGE" == "yes" ]; then
+    # Merge pull request
+    gh pr merge \
+        --auto \
+        --squash
+fi
