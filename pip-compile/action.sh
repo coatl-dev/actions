d6ada1c96c110b150b09f22515742cb4cf00e329
César Román <cesar@coatl.dev>
2025-05-05 17:16:52 -0700

    refactor(pip-compile): add ability to use config file
    pip-compile can use a configuration file (.pip-tools.toml,
    pyproject.toml or another TOML file).

--- pip-compile/action.sh
+++ pip-compile/action.sh
@@ -6,8 +6,26 @@ source /etc/profile  # Makes python and other executables findable
 
 function process_file() {
   local file="$1"
-  command=$(grep -m 1 "#    pip-compile" "$file" | sed 's/#    pip-compile/pip-compile --upgrade/')
-  eval "$command"
+  local config_file="$INPUT_CONFIG_FILE"
+  local use_config="$INPUT_USE_CONFIG"
+
+  if [ "$use_config" == "yes" ]; then
+    local config_file="${file%.txt}.in"
+    if [ -f "$config_file" ]; then
+      echo "Using config file: $config_file"
+      pip-compile --upgrade "$file" --config-file "$config_file"
+    else
+      echo "Config file not found: $config_file"
+      exit 1
+    fi
+  else
+    command=$(grep -m 1 "#    pip-compile" "$file" | sed 's/#    pip-compile/pip-compile --upgrade/')
+    if [ -n "$command" ]; then
+      eval "$command"
+    else
+      pip-compile "$file"
+    fi
+  fi
 }
 
 if [ -d "${INPUT_PATH}" ]; then
