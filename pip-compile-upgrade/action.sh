54826532ea687db6b83a68157e3f2ad9a8f300b5
César Román <cesar@coatl.dev>
2025-06-30 09:28:55 -0700

    refactor(pip-compile-upgrade): run using docker (#104)

--- pip-compile-upgrade/action.sh
+++ pip-compile-upgrade/action.sh
@@ -6,24 +6,12 @@ source /etc/profile  # Makes python and other executables findable
 
 function process_file() {
   local file="$1"
-  local use_config="$INPUT_USE_CONFIG"
-  local config_file="$INPUT_CONFIG_FILE"
   local extra_args="${INPUT_EXTRA_ARGS:-}"
 
   command=$(grep -m 1 "#    pip-compile" "$file")
   if [ -n "$command" ]; then
-    if [ "$use_config" == "yes" ]; then
-      if [ -f "$config_file" ]; then
-        file_name=$(grep -m 1 -E "#    pip-compile.*(pyproject\.toml|setup\.cfg|setup\.py|[^ ]+\.in)" "$file" | sed -E "s/.*(#    pip-compile.* (pyproject\.toml|setup\.cfg|setup\.py|[^ ]+\.in)).*/\2/")
-        upgrade_command="pip-compile --upgrade $extra_args --config $config_file $file_name"
-        eval "$upgrade_command"
-      else
-        exit 1
-      fi
-    else
-      upgrade_command=$(grep -m 1 "#    pip-compile" "$file" | sed "s/#    pip-compile/pip-compile --upgrade $extra_args/")
-      eval "$upgrade_command"
-    fi
+    upgrade_command=$(grep -m 1 "#    pip-compile" "$file" | sed "s/#    pip-compile/pip-compile --upgrade $extra_args/")
+    eval "$upgrade_command"
   else
     exit 1
   fi
