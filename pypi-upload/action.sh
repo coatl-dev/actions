5c4da887eea7e4af31ce4c332f382373cd53c11d
César Román <cesar@coatl.dev>
2025-07-01 13:30:47 -0700

    feat: add pypi-upload action (#105)
    use coatl-dev/python-tools:#.##-build Docker image

---
+++ pypi-upload/action.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# shellcheck source=/dev/null
+
+export PATH="/usr/bin:${PATH}"  # To find `id`
+source /etc/profile  # Makes python and other executables findable
+
+cd "${INPUT_WORKING_DIRECTORY}" || exit 1
+
+python -m build
+
+if [[ "${TWINE_CHECK}" == "true" ]]; then
+  python -m twine check dist/*
+fi
+
+if [[ "${INPUT_DRY_RUN}" == "true" ]]; then
+  echo "Dry run enabled, not uploading the package."
+  exit 0
+elif [[ -z "${TWINE_PASSWORD}" ]]; then
+  echo "TWINE_PASSWORD is not set."
+  exit 1
+else
+  python -m twine upload --verbose dist/*
+fi
