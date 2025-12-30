e692b255d202d2316b3329e63eb892b62dc9756b
César Román <cesar@coatl.dev>
2025-12-30 13:39:22 -0800

    fix(pip-compile): changing from tabs to four spaces for commands

--- pip-compile-upgrade/action.sh
+++ pip-compile-upgrade/action.sh
@@ -8,9 +8,9 @@ function process_file() {
 	local file="$1"
 	local extra_args="${INPUT_EXTRA_ARGS:-}"
 
-	command=$(grep -m 1 "#	pip-compile" "$file")
+	command=$(grep -m 1 "#    pip-compile" "$file")
 	if [ -n "$command" ]; then
-		upgrade_command=$(grep -m 1 "#	pip-compile" "$file" | sed "s/#	pip-compile/pip-compile --upgrade $extra_args/")
+		upgrade_command=$(grep -m 1 "#    pip-compile" "$file" | sed "s/#    pip-compile/pip-compile --upgrade $extra_args/")
 		eval "$upgrade_command"
 	else
 		exit 1
