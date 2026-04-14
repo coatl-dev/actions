c9b237b3188889669575cf517c970f43013d248e
César Román <cesar@coatl.dev>
2026-04-13 17:25:16 -0700

    revert(uv-pip-compile): rollback change to action.sh

--- uv-pip-compile-upgrade/action.sh
+++ uv-pip-compile-upgrade/action.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
 
 export PATH="/usr/bin:${PATH}"  # To find `id`
 # shellcheck source=/dev/null
@@ -8,22 +6,8 @@ source /etc/profile  # Makes python and other executables findable
 
 function process_file() {
 	local file="$1"
-	local raw_command
-	raw_command=$(grep -m 1 '^#[[:space:]]*uv pip compile' "$file" | sed 's/^#[[:space:]]*//') || true
-	if [ -z "$raw_command" ]; then
-		exit 1
-	fi
-
-	local -a command_parts
-	read -r -a command_parts <<< "$raw_command"
-	if [ "${command_parts[0]:-}" != "uv" ]; then
-		exit 1
-	fi
-
-	local -a command
-	command=("${command_parts[@]}" "--upgrade")
-
-	"${command[@]}"
+	command=$(grep -m 1 "#    uv pip compile" "$file" | sed "s/#    uv pip compile/uv pip compile --upgrade/")
+	eval "$command"
 }
 
 cd "${INPUT_WORKING_DIRECTORY}" || exit 1
